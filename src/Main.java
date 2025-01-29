be20ceb0fd8261871a816c6be1e779519393eb0f
Olivia Kamau <148930291+OliviaKamau@users.noreply.github.com>
2025-01-29 14:16:27 -0500

    Added 4th print statement

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello universe!");
         System.out.println("Hello galaxy!");
+        System.out.println("Hello milky way!");
 
     }
 }
